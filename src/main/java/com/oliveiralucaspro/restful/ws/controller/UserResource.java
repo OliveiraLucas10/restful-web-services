e1cd075f124bcf5db8a1e9e3fa0bfe2df10bec22
OliveiraLucas10 <oliveiraborgeslucas@gmail.com>
2020-05-25 21:22:40 +0100

    add post

--- src/main/java/com/oliveiralucaspro/restful/ws/controller/UserResource.java
+++ src/main/java/com/oliveiralucaspro/restful/ws/controller/UserResource.java
@@ -1,12 +1,16 @@
 package com.oliveiralucaspro.restful.ws.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.oliveiralucaspro.restful.ws.domain.User;
 import com.oliveiralucaspro.restful.ws.domain.UserDaoService;
@@ -18,23 +22,27 @@ import lombok.RequiredArgsConstructor;
 public class UserResource {
 
     private final UserDaoService service;
-    
+
     // retrieve all users
     @GetMapping("/users")
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
 	return service.findAll();
     }
-    
-    
+
     // retrieve user by id
     @GetMapping("/users/{id}")
     public User getUserById(@PathVariable int id) {
 	return service.finOne(id);
     }
-    
+
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
 	User savedUser = service.save(user);
+
+	URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId())
+		.toUri();
+
+	return ResponseEntity.created(location).build();
     }
-    
+
 }
