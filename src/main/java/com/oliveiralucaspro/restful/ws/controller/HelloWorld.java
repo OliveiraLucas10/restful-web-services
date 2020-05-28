6574690df987ad1778dbe2e8516986142c639b40
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-28 07:51:21 +0100

    add i18N

--- src/main/java/com/oliveiralucaspro/restful/ws/controller/HelloWorld.java
+++ src/main/java/com/oliveiralucaspro/restful/ws/controller/HelloWorld.java
@@ -1,16 +1,24 @@
 package com.oliveiralucaspro.restful.ws.controller;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.oliveiralucaspro.restful.ws.domain.HelloWorldBean;
 
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloWorld {
 
+	private final MessageSource messageSource;
+	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World!!";
@@ -19,5 +27,10 @@ public class HelloWorld {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World!!");
+	}
+	
+	@GetMapping(path = "/hello-world-i18n")
+	public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good,morning.message", null, locale);
 	}
 }
