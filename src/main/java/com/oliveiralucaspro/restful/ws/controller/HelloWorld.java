0aea9dbe353888a9f9a8778e967b7671b200d6d4
Lucas Oliveira <oliveiraborgeslucas@gmail.com>
2020-05-28 07:58:22 +0100

    new configuration to simplify use of i18n

--- src/main/java/com/oliveiralucaspro/restful/ws/controller/HelloWorld.java
+++ src/main/java/com/oliveiralucaspro/restful/ws/controller/HelloWorld.java
@@ -3,6 +3,7 @@ package com.oliveiralucaspro.restful.ws.controller;
 import java.util.Locale;
 
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,5 +33,10 @@ public class HelloWorld {
 	@GetMapping(path = "/hello-world-i18n")
 	public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
 		return messageSource.getMessage("good,morning.message", null, locale);
+	}
+	
+	@GetMapping(path = "/hello-world-i18n2")
+	public String helloWorldInternationalized2() {
+		return messageSource.getMessage("good,morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
